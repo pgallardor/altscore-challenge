1e2aba21714e197159ba68091dc7e8ad73962c7b
Pedro Gallardo <pegallardorobinson@gmail.com>
2024-08-30 13:14:17 -0400

    commit for alt challenge

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import express, { type Request, type Response, type Express } from "express"
+import {readFileSync} from "fs"
+
+const app: Express = express()
+
+enum HttpCode {
+    OK = 200,
+    TEAPOT = 418
+}
+
+app.get("/", (req: Request, res: Response) => {
+    return res.status(HttpCode.OK).send("Helloo!!!")
+})
+
+app.get("/status", (req: Request, res: Response) => {
+    return res.status(HttpCode.OK).json({
+        "damaged_system": "life_support"
+    })
+})
+
+app.get("/repair-bay", (req: Request, res: Response) => {
+    res.setHeader("Content-Type", "text/html")
+    const html = readFileSync("index.html")
+    res.write(html)
+    res.end()
+})
+
+app.post("/teapot", (req: Request, res: Response) => {
+    return res.status(HttpCode.TEAPOT)
+})
+
+app.listen(8080, () => {
+    console.log("Running on port 8080")
+})
